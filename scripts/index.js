aa96b49711882aac35be755e18ba401fb10127e9
Naresh <68911787+nareshpoonia@users.noreply.github.com>
2022-02-11 22:15:57 +0530

    Added Avatar Component

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,3 @@
-const mainMenu = document.querySelector(".main-menu");
-const closeMenu = document.querySelector(".close-menu");
-const openMenu = document.querySelector(".open-menu");
-
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav-menu");
 
@@ -11,15 +7,3 @@ let showMenu = () => {
 };
 
 hamburger.addEventListener("click", showMenu);
-
-let show = () => {
-  mainMenu.style.display = "flex";
-  openMenu.style.display = "none";
-  closeMenu.style.display = "block";
-};
-
-let close = () => {
-  mainMenu.style.display = "none";
-  closeMenu.style.display = "none";
-  openMenu.style.display = "flex";
-};
