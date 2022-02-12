b7b2fe2e653ac09c999c7fe49f5e5206daf20a5f
Naresh <68911787+nareshpoonia@users.noreply.github.com>
2022-02-12 10:32:09 +0530

    Fixed navigation & layout issue

--- scripts/docs.js
+++ scripts/docs.js
@@ -1,28 +1,24 @@
 const mainMenu = document.querySelector(".main-menu");
 const closeMenu = document.querySelector(".close-menu");
 const openMenu = document.querySelector(".open-menu");
+
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav-menu");
-
-hamburger.addEventListener("click", showMenu);
 
 let showMenu = () => {
   hamburger.classList.toggle("active");
   navMenu.classList.toggle("active");
 };
 
+hamburger.addEventListener("click", showMenu);
+
 let show = () => {
   mainMenu.style.display = "flex";
   openMenu.style.display = "none";
   closeMenu.style.display = "block";
 };
-
 let close = () => {
   mainMenu.style.display = "none";
   closeMenu.style.display = "none";
   openMenu.style.display = "flex";
 };
-
-openMenu.addEventListener("click", show);
-
-closeMenu.addEventListener("click", close);
--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,6 @@
+const closeMenu = document.querySelector(".close-menu");
+const openMenu = document.querySelector(".open-menu");
+
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav-menu");
 
@@ -7,3 +10,14 @@ let showMenu = () => {
 };
 
 hamburger.addEventListener("click", showMenu);
+
+let show = () => {
+  mainMenu.style.display = "flex";
+  openMenu.style.display = "none";
+  closeMenu.style.display = "block";
+};
+let close = () => {
+  mainMenu.style.display = "none";
+  closeMenu.style.display = "none";
+  openMenu.style.display = "flex";
+};
